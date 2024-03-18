92478970e816516d7f5762c17a5900f467a4e459
Varun Kumar <varun.raj874@gmail.com>
2024-03-18 19:15:50 +0530

    Wordladder code modified

--- src/main/java/WordLadder.java
+++ src/main/java/WordLadder.java
@@ -44,77 +44,116 @@ public class WordLadder {
 	public static void main(String[] args) {
 		//#region Inputs
 		String beginWord = "hit", endWord = "cog";
-		String[] wordList = {"hot", "dot", "dog", "lot", "log", "sot", "sog", "hat", "cog"};
+		String[] words = {"hot","dot","dog","lot","log","cog"};
 		//#endregion
 
-		System.out.println(minWordsInShortestPath(beginWord, endWord, wordList));
-		//#region Code
-
+		System.out.println(ladderLength(beginWord, endWord, Arrays.asList(words)));
 	}
 
-	public static int minWordsInShortestPath(String beginWord, String endWord, String[] wordsArr) {
-		Set<String> words = new HashSet<>(Arrays.asList(wordsArr));
+	/**
+	 * Get the number of words
+	 * @param beginWord Starting word, It may or may not be available in wordsArr
+	 * @param endWord End word
+	 * @param wordList Words arr
+	 * @return No. of words in the shortest path from begin word to end word
+	 */
+	public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+		if(! wordList.contains(endWord)) return 0;		// When endWord is not in array
+
+		Set<String> words = new HashSet<>(wordList);
 		Queue<String> visited = new LinkedList<>();
 		visited.add(beginWord);
-		if (! words.contains(endWord)) return 0;
-		int level = 0;
-		Map<String, Set<String>> adjacencyList = new HashMap<>();
-
-		while (!visited.isEmpty()) {
-			level++;
-
+		int numberOfWords = 0;
+		while (! visited.isEmpty()) {
+			numberOfWords++;
 			int nodesAtCurrentLevel = visited.size();
-			for (int k = 0; k < nodesAtCurrentLevel; k++) {
+			for (int j = 0; j < nodesAtCurrentLevel; j++) {
 				String currentWord = visited.poll();
-				if(currentWord == null) continue;
+				char[] currentWordChars = currentWord.toCharArray();		// currentWord will be always != null here
+				for (int i =0 ; i< currentWordChars.length; i++) {
+					char currentChar = currentWordChars[i];
+					for (char c = 'a'; c<= 'z'; c++) {
+						currentWordChars[i] = c;
 
-				Set<String> adjacentNodes = new HashSet<>();
-				adjacencyList.put(currentWord, adjacentNodes);
-
-				char[] chars = currentWord.toCharArray();
-
-				for (int i = 0; i < chars.length; i++) {
-					for (char j = 'a'; j < 'z'; j++) {
-						char currentChar = chars[i];
-						chars[i] = j;
-						String newWord = String.valueOf(chars);
-						if(words.contains(newWord)) {
-							if(newWord.equals(endWord)) {
-								print(beginWord, endWord, currentWord, adjacencyList);
-								return level + 1;
+						if(words.contains(String.valueOf(currentWordChars))) {
+							if(String.valueOf(currentWordChars).equals(endWord)) {
+								return ++numberOfWords;
 							}
-							adjacentNodes.add(newWord);
-							visited.add(newWord);
-							words.remove(newWord);
+							visited.add(String.valueOf(currentWordChars));
+							words.remove(String.valueOf(currentWordChars));
 						}
-						chars[i] = currentChar;
 					}
+					currentWordChars[i] = currentChar;
 				}
 			}
 		}
 		return 0;
 	}
 
-	static void print(String beginWord, String endWord, String prevWord, Map<String, Set<String>> adjacencyList) {
-		Stack<String> words = new Stack<>();
-		words.push(endWord);
-		words.push(prevWord);
 
-		while (!words.contains(beginWord)) {
-			String current = words.peek();
-			for (var entry: adjacencyList.entrySet()) {
-				if(entry.getValue().contains(current)) {
-					words.push(entry.getKey());
-					break;
-				}
-			}
-		}
-
-		while (!words.isEmpty()) {
-			System.out.print(words.pop());
-			if (!words.isEmpty()) System.out.print("->");
-		}
-		System.out.println();
-	}
+//	public static int minWordsInShortestPath(String beginWord, String endWord, String[] wordsArr) {
+//		Set<String> words = new HashSet<>(Arrays.asList(wordsArr));
+//		Queue<String> visited = new LinkedList<>();
+//		visited.add(beginWord);
+//		if (! words.contains(endWord)) return 0;
+//		int level = 0;
+//		Map<String, Set<String>> adjacencyList = new HashMap<>();
+//
+//		while (!visited.isEmpty()) {
+//			level++;
+//
+//			int nodesAtCurrentLevel = visited.size();
+//			for (int k = 0; k < nodesAtCurrentLevel; k++) {
+//				String currentWord = visited.poll();
+//				if(currentWord == null) continue;
+//
+//				Set<String> adjacentNodes = new HashSet<>();
+//				adjacencyList.put(currentWord, adjacentNodes);
+//
+//				char[] chars = currentWord.toCharArray();
+//
+//				for (int i = 0; i < chars.length; i++) {
+//					for (char j = 'a'; j < 'z'; j++) {
+//						char currentChar = chars[i];
+//						chars[i] = j;
+//						String newWord = String.valueOf(chars);
+//						if(words.contains(newWord)) {
+//							if(newWord.equals(endWord)) {
+//								print(beginWord, endWord, currentWord, adjacencyList);
+//								return level + 1;
+//							}
+//							adjacentNodes.add(newWord);
+//							visited.add(newWord);
+//							words.remove(newWord);
+//						}
+//						chars[i] = currentChar;
+//					}
+//				}
+//			}
+//		}
+//		return 0;
+//	}
+//
+//	static void print(String beginWord, String endWord, String prevWord, Map<String, Set<String>> adjacencyList) {
+//		Stack<String> words = new Stack<>();
+//		words.push(endWord);
+//		words.push(prevWord);
+//
+//		while (!words.contains(beginWord)) {
+//			String current = words.peek();
+//			for (var entry: adjacencyList.entrySet()) {
+//				if(entry.getValue().contains(current)) {
+//					words.push(entry.getKey());
+//					break;
+//				}
+//			}
+//		}
+//
+//		while (!words.isEmpty()) {
+//			System.out.print(words.pop());
+//			if (!words.isEmpty()) System.out.print("->");
+//		}
+//		System.out.println();
+//	}
 
 }
